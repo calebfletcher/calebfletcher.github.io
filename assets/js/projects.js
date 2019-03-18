c9915990f62f09e783ef41e3a293bdcc2405ab42
Caleb Fletcher <caleb@fletcher.cf>
2019-03-18 16:05:27 +1100

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,25 +1,26 @@
 (function() {
     const numberOfRepos = 4
-    let container = document.getElementById("projects-container")
-    let ul = document.createElement("ul")
-    ul.classList.add("features")
+    let container = document.getElementById('projects-container')
+    let ul = document.createElement('ul')
+    ul.classList.add('features')
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             let githubData = JSON.parse(this.responseText)
             githubData = (githubData.length > numberOfRepos) ? githubData.slice(0, numberOfRepos) : githubData
             for (const repo of githubData) {
-                const forkString = repo.fork ? " (Fork)" : ""
+                const forkString = repo.fork ? ' (Fork)' : ''
+                const descriptionString = repo.description ? `<p>${repo.description}</p>` : ''
                 let el = document.createElement("li")
                 let a = document.createElement("a")
                 a.href = repo.html_url
-                a.innerHTML = "<h3>" + repo.name + forkString + "</h3><p>" + repo.description + "</p>"
+                a.innerHTML = `<h3>${repo.name}${forkString}</h3>${descriptionString}`
                 el.appendChild(a)
                 ul.appendChild(el)
             }
             container.appendChild(ul)
         }
     };
-    xhttp.open("GET", "https://api.github.com/users/calebfletcher/repos?sort=updated", true);
+    xhttp.open('GET', 'https://api.github.com/users/calebfletcher/repos?sort=updated', true);
     xhttp.send();
 })()
