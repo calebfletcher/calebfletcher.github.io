7ba59b07475c7d211b5b81d62cc37f6afb90b502
Caleb Fletcher <caleb@fletcher.cf>
2023-03-06 09:53:50 +1100

    clean up repos list

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,14 +1,23 @@
-(function() {
+(function () {
     const numberOfRepos = 4
     let container = document.getElementById('projects-container')
     let ul = document.createElement('ul')
     ul.classList.add('features')
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             let githubData = JSON.parse(this.responseText)
-            githubData = (githubData.length > numberOfRepos) ? githubData.slice(0, numberOfRepos) : githubData
-            for (const repo of githubData) {
+            let i = 0;
+            while (i < numberOfRepos) {
+                const repo = githubData[i]
+
+                if (repo.fork) {
+                    continue
+                }
+                if (repo.name === "calebfletcher.github.io") {
+                    continue
+                }
+
                 const descriptionString = (repo.description != null) ? `<p>${repo.description}</p>` : ''
                 let el = document.createElement("li")
                 let a = document.createElement("a")
@@ -16,6 +25,7 @@
                 a.innerHTML = `<h3>${repo.name}</h3>${descriptionString}`
                 el.appendChild(a)
                 ul.appendChild(el)
+                i++
             }
             container.appendChild(ul)
         }
