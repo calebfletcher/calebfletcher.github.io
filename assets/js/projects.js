cb35e6f6e127fe8634e0db833eff07f254e32d98
Caleb Fletcher <caleb@fletcher.cf>
2023-03-06 10:05:13 +1100

    fix loop bug

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -4,35 +4,35 @@
     let ul = document.createElement('ul')
     ul.classList.add('features')
     var xhttp = new XMLHttpRequest();
-    // xhttp.onreadystatechange = function () {
-    //     if (this.readyState == 4 && this.status == 200) {
-    //         let githubData = JSON.parse(this.responseText)
-    //         let i = 0;
-    //         while (i < numberOfRepos) {
-    //             if (i >= githubData.length) {
-    //                 break
-    //             }
-    //             const repo = githubData[i]
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let githubData = JSON.parse(this.responseText)
+            let i = 0;
+            while (i < numberOfRepos) {
+                if (i >= githubData.length) {
+                    break
+                }
+                const repo = githubData[i]
+                i++;
 
-    //             if (repo.fork) {
-    //                 continue
-    //             }
-    //             if (repo.name === "calebfletcher.github.io") {
-    //                 continue
-    //             }
+                if (repo.fork) {
+                    continue
+                }
+                if (repo.name === "calebfletcher.github.io") {
+                    continue
+                }
 
-    //             const descriptionString = (repo.description != null) ? `<p>${repo.description}</p>` : ''
-    //             let el = document.createElement("li")
-    //             let a = document.createElement("a")
-    //             a.href = repo.html_url
-    //             a.innerHTML = `<h3>${repo.name}</h3>${descriptionString}`
-    //             el.appendChild(a)
-    //             ul.appendChild(el)
-    //             i++
-    //         }
-    //         container.appendChild(ul)
-    //     }
-    // };
+                const descriptionString = (repo.description != null) ? `<p>${repo.description}</p>` : ''
+                let el = document.createElement("li")
+                let a = document.createElement("a")
+                a.href = repo.html_url
+                a.innerHTML = `<h3>${repo.name}</h3>${descriptionString}`
+                el.appendChild(a)
+                ul.appendChild(el)
+            }
+            container.appendChild(ul)
+        }
+    };
     xhttp.open('GET', 'https://api.github.com/users/calebfletcher/repos?sort=updated', true);
     xhttp.send();
 })()
